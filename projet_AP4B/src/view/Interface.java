c9e91c385e3e9e4883fe14261afd390f04b64a5b
Franklin7660 <133520356+Franklin7660@users.noreply.github.com>
2023-05-31 08:21:19 +0200

    fr

--- projet_AP4B/src/view/Interface.java
+++ projet_AP4B/src/view/Interface.java
@@ -12,22 +12,24 @@ public class Interface extends JFrame {
     public Interface() {
         setTitle("My Application");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(1000, 1000);
+        setSize(1280, 720);
         setLocationRelativeTo(null);
         graph = new Graph();
 
         getContentPane().setLayout(null);
-        zoomIN = new JButton("ZoomIN");
 
-        zoomIN.setBounds(50,750,100,20);
+        zoomIN = new JButton("ZoomIN");
+        zoomIN.setBounds(50,50,100,20);
         zoomIN.addActionListener(e -> graph.zoomIn());
+
         zoomOUT = new JButton("ZoomOUT");
-        zoomOUT.setBounds(50,650,100,20);
+        zoomOUT.setBounds(50,150,100,20);
         zoomOUT.addActionListener(e -> graph.zoomOut());
+
         int x = (getWidth()-500) / 2;
         int y = (getHeight() - 500) / 2;
 
-        graph.setBounds(x,0,500,500);
+        graph.setBounds(x,y,500,500);
         getContentPane().add(graph);
         getContentPane().add(zoomIN);
         getContentPane().add(zoomOUT);
